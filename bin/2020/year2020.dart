677af3f14047edfb4a779d8fa35aaa14f8703088
Praful Kapadia <git@kaptech.co.uk>
2020-12-04 15:20:44 +0000

    day 4, part 1 done

--- bin/2020/year2020.dart
+++ bin/2020/year2020.dart
@@ -1,16 +1,23 @@
 import './day01.dart';
 import './day02.dart';
 import './day03.dart';
+import './day04.dart';
 
 void run() {
-  test3b();
-  day3b();
-  test3a();
-  day3a();
+  test4b();
+  day4b();
+  test4a();
+  day4a();
+
+  // test3b();
+  // day3b();
+  // test3a();
+  // day3a();
+
+  // day2a();
+  // day2b();
 
   // day1a();
   // day1b();
   // test1();
-  // day2a();
-  // day2b();
 }
