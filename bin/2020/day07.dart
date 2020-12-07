cf412f6153329aef74488709c2c9b9ed861789ba
Praful Kapadia <git@kaptech.co.uk>
2020-12-07 18:08:56 +0000

    day 7, part 2 done

--- bin/2020/day07.dart
+++ bin/2020/day07.dart
@@ -4,39 +4,64 @@ import '../utils.dart';
 const bool DEBUG = false;
 
 final String TEST_INPUT = File('./data/day07-test.txt').readAsStringSync();
+final String TEST_INPUT2 = File('./data/day07b-test.txt').readAsStringSync();
 
 final String MAIN_INPUT = File('./data/day07.txt').readAsStringSync();
 
-Iterable<String> findOuterBag(input, bag) {
-  var outerBagRegex =
-      RegExp(r'(?<outerBag>.*)s contain.*' + bag, multiLine: true);
+Iterable<String> findOuterBags(input, bag) {
   var result = <String>{};
-  outerBagRegex.allMatches(input).forEach((match) {
+  RegExp(r'(?<outerBag>.*)s contain.*' + bag, multiLine: true)
+      .allMatches(input)
+      .forEach((match) {
     var outerBag = match.namedGroup('outerBag');
     result.add(outerBag);
-    result.addAll(findOuterBag(input, outerBag));
+    result.addAll(findOuterBags(input, outerBag));
   });
   return result;
 }
 
-void part1Test() {
+int findInnerBagCount(input, bag) {
+  var result = 0;
+  RegExp(bag + r's contain (?<innerBags>.*).', multiLine: true)
+      .allMatches(input)
+      .forEach((match) {
+    match.namedGroup('innerBags').split(',').forEach((innerBags) {
+      RegExp(r'(?<count>\d+) (?<name>.*bag)s?')
+          .allMatches(innerBags)
+          .forEach((bag) {
+        var count = int.parse(bag.namedGroup('count'));
+        var innerInnerBagCount =
+            findInnerBagCount(input, bag.namedGroup('name'));
+        result += count + (count * innerInnerBagCount);
+      });
+    });
+  });
+  return result;
+}
+
+void test() {
   printHeader('7a test');
   //Answer: 4
-  print(findOuterBag(TEST_INPUT, 'shiny gold bag').length);
+  print(findOuterBags(TEST_INPUT, 'shiny gold bag').length);
+  //Answer: 32
+  print(findInnerBagCount(TEST_INPUT, 'shiny gold bag'));
+  //Answer: 126
+  print(findInnerBagCount(TEST_INPUT2, 'shiny gold bag'));
 }
 
 void part1() {
   //Answer: 115
   printHeader('7a');
-  print(findOuterBag(MAIN_INPUT, 'shiny gold bag').length);
+  print(findOuterBags(MAIN_INPUT, 'shiny gold bag').length);
 }
 
 void part2() {
   printHeader('7b');
+  print(findInnerBagCount(MAIN_INPUT, 'shiny gold bag'));
 }
 
 void main(List<String> arguments) {
-  part1Test();
+  test();
   part1();
   part2();
 }
