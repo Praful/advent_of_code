cd21363f58b4913053ec9cf6242c36d7f057019d
Praful Kapadia <git@kaptech.co.uk>
2020-12-15 23:01:12 +0000

    day 15 part 2 done

--- 2020/src/day15.dart
+++ 2020/src/day15.dart
@@ -1,56 +1,50 @@
 import './utils.dart';
 
-const bool DEBUG = false;
-const int TURN_COUNT = 2020;
-// const int TURN_COUNT = 30000000;
+const bool DEBUG = true;
+const int TURN_COUNT_1 = 2020;
+const int TURN_COUNT_2 = 30000000;
 // const int TURN_COUNT = 11;
 
-void runPart1(String name, List<int> input) {
+void findLastSpokenNumber(String name, List<int> input,
+    [turnCount = TURN_COUNT_1]) {
   printHeader(name);
 
-  var history = <int, List<int>>{};
-  for (var i = 0; i < input.length - 1; i++) history[input[i]] = [i + 1];
+  var history = <int, int>{};
+  for (var i = 0; i < input.length - 1; i++) history[input[i]] = i + 1;
 
   var lastNumberSpoken = input.last;
-  for (var turn = input.length + 1; turn <= TURN_COUNT; turn++) {
-    if (DEBUG) print('$turn: last number is $lastNumberSpoken');
+  for (var turn = input.length + 1; turn <= turnCount; turn++) {
+    var occurrences = history[lastNumberSpoken] ?? 0;
 
-    var occurrences = history[lastNumberSpoken] ?? [];
-
-    if (occurrences.isEmpty) {
-      occurrences.add(turn - 1);
-      history[lastNumberSpoken] = occurrences;
+    if (occurrences == 0) {
+      history[lastNumberSpoken] = turn - 1;
       lastNumberSpoken = 0;
     } else {
-      lastNumberSpoken = (turn - 1) - occurrences.max;
-      occurrences.add(turn - 1);
+      history[lastNumberSpoken] = turn - 1;
+      lastNumberSpoken = (turn - 1) - occurrences;
     }
-    if (DEBUG) print('--- speak $lastNumberSpoken');
   }
-  if (DEBUG) print(history);
   print(lastNumberSpoken);
 }
 
-void runPart2(String name, List input) {
-  printHeader(name);
-}
-
 void main(List<String> arguments) {
-  //Answer: 436
-  runPart1('15 test part 1', [0, 3, 6]);
+  if (DEBUG) {
+    findLastSpokenNumber('15 test part 1', [0, 3, 6]); //: 436
 
-  runPart1('15 test part 1a', [1, 3, 2]); // 2020th number spoken is 1.
-  runPart1('15 test part 1b', [2, 1, 3]); // 2020th number spoken is 10.
-  runPart1('15 test part 1c', [1, 2, 3]); // 2020th number spoken is 27.
-  runPart1('15 test part 1d', [2, 3, 1]); // 2020th number spoken is 78.
-  runPart1('15 test part 1e', [3, 2, 1]); // 2020th number spoken is 438.
-  runPart1('15 test part 1f', [3, 1, 2]); // 2020th number spoken is 1836.
-
-  //Answer:
-  // runPart2('15 test part 2', TEST_INPUT);
-
-  //Answer: 1696
-  runPart1('15 part 1', [12, 1, 16, 3, 11, 0]);
-  //Answer:
-  // runPart2('15 part 2', MAIN_INPUT);
+    findLastSpokenNumber(
+        '15 test part 1a', [1, 3, 2]); // 2020th number spoken is 1.
+    findLastSpokenNumber(
+        '15 test part 1b', [2, 1, 3]); // 2020th number spoken is 10.
+    findLastSpokenNumber(
+        '15 test part 1c', [1, 2, 3]); // 2020th number spoken is 27.
+    findLastSpokenNumber(
+        '15 test part 1d', [2, 3, 1]); // 2020th number spoken is 78.
+    findLastSpokenNumber(
+        '15 test part 1e', [3, 2, 1]); // 2020th number spoken is 438.
+    findLastSpokenNumber(
+        '15 test part 1f', [3, 1, 2]); // 2020th number spoken is 1836.
+  }
+  //Answer: part 1: 1696, part 2: 37385
+  findLastSpokenNumber('15 part 1', [12, 1, 16, 3, 11, 0], TURN_COUNT_1);
+  findLastSpokenNumber('15 part 2', [12, 1, 16, 3, 11, 0], TURN_COUNT_2);
 }
