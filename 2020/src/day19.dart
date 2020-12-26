69c50c9ca4d52113c4b8807060962fa283ee7124
Praful Kapadia <git@kaptech.co.uk>
2020-12-26 00:01:57 +0000

    day 19 part 2 and utils updated

--- 2020/src/day19.dart
+++ 2020/src/day19.dart
@@ -11,7 +11,7 @@ enum RuleType { nonterminal, terminal }
 class Rule {
   RuleType type;
   String value;
-  List ruleIds = [];
+  List mappings = [];
 
   Rule(String input) {
     if (input.contains('"')) {
@@ -20,12 +20,12 @@ class Rule {
     } else {
       type = RuleType.nonterminal;
       var options = input.split('|');
-      options.forEach((r) => ruleIds.add(r.trim().split(' ')));
+      options.forEach((r) => mappings.add(r.trim().split(' ').map(int.parse)));
     }
   }
   @override
   String toString() {
-    return (type == RuleType.nonterminal ? ruleIds.toString() : value);
+    return (type == RuleType.nonterminal ? mappings.toString() : value);
   }
 }
 
@@ -46,7 +46,7 @@ class Rules {
       }
       if (isRule) {
         var parts = s.split(':');
-        var id = parts[0];
+        var id = int.parse(parts[0]);
         rules[id] = Rule(parts[1]);
       } else {
         if (s.isNotEmpty) messages.add(s);
@@ -54,15 +54,34 @@ class Rules {
     });
   }
 
-  Object createRegex([start = '0']) {
-    var top = rules[start];
-    if (top.type == RuleType.terminal) return top.value;
-    return top.ruleIds.map((ids) => ids
-        .map((id) => createRegex(id))
-        .toList()
-        .map((alts) => alts.length == 1
-            ? alts
-            : "(${alts.map((r) => r.join()).join('|')})"));
+  Object rule8() => '(${createRegex(42, true)})+';
+  Object rule11() {
+    var r42 = '(${createRegex(42, true)})';
+    var r31 = '(${createRegex(31, true)})';
+
+    //HACK! to create repeating groups since Dart doesn't seem
+    //to support. The 10 is chose based on part 2 rules. Other
+    //rules/data may not work.
+    var r8 = range(1, 10).map((i) => '$r42{$i}$r31{$i}').toList();
+
+    return '(?:${r8.join('|')})';
+  }
+
+  Object createRegex([start = 0, part2 = false]) {
+    if (part2) {
+      if (start == 8) return rule8();
+      if (start == 11) return rule11();
+    }
+
+    var rule = rules[start];
+
+    if (rule.type == RuleType.terminal) return rule.value;
+
+    var regex = rule.mappings.map((alternatives) =>
+        alternatives.map((id) => createRegex(id, part2)).join());
+
+    //Use non-capturing groups (?:) otherwise it's slow.
+    return '(?:${regex.join('|')})';
   }
 
   @override
@@ -71,19 +90,15 @@ class Rules {
   }
 }
 
-void runPart1(String name, List input) {
+void runPart(String name, List input, [isPart2 = false]) {
   printHeader(name);
-  var rules = Rules(input);
-  var rulesRegexStr =
-      '^' + rules.createRegex().toString().replaceAll(', ', '') + '\$';
-  var regex = RegExp(rulesRegexStr);
-  var result =
-      rules.messages.fold(0, (acc, m) => acc + (regex.hasMatch(m) ? 1 : 0));
-  print(result);
-}
 
-void runPart2(String name, List input) {
-  printHeader(name);
+  var rules = Rules(input);
+  var rulesRegexStr = '^${rules.createRegex(0, isPart2)}\$';
+  var regexRule0 = RegExp(rulesRegexStr);
+  var result = rules.messages
+      .fold(0, (acc, m) => acc + (regexRule0.hasMatch(m) ? 1 : 0));
+  print(result);
 }
 
 void main(List<String> arguments) {
@@ -92,12 +107,12 @@ void main(List<String> arguments) {
   MAIN_INPUT = fileAsString('../data/day19.txt');
 
   //Answer: 2
-  runPart1('19 test part 1', TEST_INPUT);
+  runPart('19 test part 1', TEST_INPUT);
   //Answer:
-  // runPart2('19 test part 2', TEST_INPUT2);
+  runPart('19 test part 2', TEST_INPUT2, true);
 
   //Answer: 165
-  runPart1('19 part 1', MAIN_INPUT);
-  //Answer:
-  // runPart2('19 part 2', MAIN_INPUT);
+  runPart('19 part 1', MAIN_INPUT);
+  //Answer: 274
+  runPart('19 part 2', MAIN_INPUT, true);
 }
