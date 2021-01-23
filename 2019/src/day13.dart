62b5194168567d9b23fe5e0bf8e0fc4ef5e49e88
Praful Kapadia <git@kaptech.co.uk>
2021-01-23 18:50:50 +0000

    2019 day 13 part 2 done

--- 2019/src/day13.dart
+++ 2019/src/day13.dart
@@ -1,6 +1,7 @@
 import '../../shared/dart/src/utils.dart';
 import './intcode_computer.dart';
 import 'dart:math';
+import 'package:console/console.dart';
 
 /// Puzzle description: https://adventofcode.com/2019/day/4
 
@@ -15,10 +16,46 @@ void printAndAssert(actual, [expected]) {
 }
 
 enum Tile { empty, wall, block, paddle, ball }
-const X_INDEX = 0;
-const Y_INDEX = 1;
-const TILE_ID_INDEX = 2;
-const OUTPUT_GROUP = 3;
+const X = 0;
+const Y = 1;
+const TILE = 2;
+const INSTRUCTION_LENGTH = 3;
+
+class Game {
+  static const MOVE_LEFT = -1;
+  static const MOVE_RIGHT = 1;
+  static const MOVE_NONE = 0;
+
+  final input;
+  List<List<int>> output;
+  Point ballPosition;
+  Point paddlePosition;
+
+  Game(this.input);
+
+  int inputProvider() => (ballPosition.x - paddlePosition.x).sign;
+
+  int play() {
+    input[0] = 2; //initialise game
+    int score;
+    var c = Computer(input, () => inputProvider());
+    while (!c.halted) {
+      c.run([], true);
+      var output = c.output();
+      if (output.length == INSTRUCTION_LENGTH) {
+        c.output(true); //clear output
+        if (output[TILE] == Tile.ball.index) {
+          ballPosition = Point(output[X], output[Y]);
+        } else if (output[TILE] == Tile.paddle.index) {
+          paddlePosition = Point(output[X], output[Y]);
+        } else if (output[X] == -1 && output[Y] == 0) {
+          score = output[TILE];
+        }
+      }
+    }
+    return score;
+  }
+}
 
 Object part1(String header, List input) {
   printHeader(header);
@@ -27,10 +64,10 @@ Object part1(String header, List input) {
   while (!c.halted) {
     c.run([], true);
     var output = c.output();
-    if (output.length == OUTPUT_GROUP) {
+    if (output.length == INSTRUCTION_LENGTH) {
       c.output(true); //clear output
-      if (output[TILE_ID_INDEX] == Tile.block.index) {
-        blockTiles.add(Point(output[X_INDEX], output[Y_INDEX]));
+      if (output[TILE] == Tile.block.index) {
+        blockTiles.add(Point(output[X], output[Y]));
       }
     }
   }
@@ -39,14 +76,12 @@ Object part1(String header, List input) {
 
 Object part2(String header, List input) {
   printHeader(header);
-
-  //TODO return something
-  return null;
+  return Game(input).play();
 }
 
 void main(List<String> arguments) {
   List mainInput = FileUtils.asInt('../data/day13.txt', ',');
 
   printAndAssert(part1('13 part 1', mainInput), 273);
-  // printAndAssert(part2('13 part 2', mainInput));
+  printAndAssert(part2('13 part 2', mainInput), 13140);
 }
