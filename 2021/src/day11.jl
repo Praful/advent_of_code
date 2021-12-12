e8938a72a033528a60769114d2e7da225c014ed0
Praful Kapadia <git@kaptech.co.uk>
2021-12-12 01:21:21 +0000

    2021 day 11 done

---
+++ 2021/src/day11.jl
@@ -0,0 +1,83 @@
+using DelimitedFiles
+# using Memoize
+# using Combinatorics
+
+# Puzzle description: https://adventofcode.com/2021/day/11
+
+
+function adj_coords(coord)
+  r, c = Tuple(coord)
+  [
+    CartesianIndex(r - 1, c),
+    CartesianIndex(r + 1, c),
+    CartesianIndex(r, c + 1),
+    CartesianIndex(r, c - 1),
+    CartesianIndex(r - 1, c + 1),
+    CartesianIndex(r - 1, c - 1),
+    CartesianIndex(r + 1, c + 1),
+    CartesianIndex(r + 1, c - 1)
+  ]
+end
+
+function flash(octopuses, has_flashed = Vector())
+  octopus_coords = keys(octopuses)
+  flashers = setdiff(findall(.>(9), octopuses), has_flashed)
+
+  if length(flashers) > 0
+    for coord in flashers
+      adj = intersect(adj_coords(coord), octopus_coords)
+      octopuses[CartesianIndex.(adj)] .+= 1
+      push!(has_flashed, coord)
+    end
+    flash(octopuses, has_flashed)
+  end
+  length(has_flashed)
+end
+
+function part1(input, steps = 100)
+  octopuses = copy(input)
+  flash_count = 0
+
+  for _ = 1:steps
+    octopuses .+= 1
+    flash_count += flash(octopuses)
+    octopuses[octopuses.>9] .= 0
+  end
+  @show flash_count
+end
+
+function part2(input)
+  octopuses = copy(input)
+  step = 0
+
+  while true
+    step += 1
+    octopuses .+= 1
+    flash(octopuses)
+    flashed = (octopuses[octopuses.>9] .= 0)
+    if length(flashed) == length(octopuses)
+      return step
+    end
+  end
+end
+
+function read_input(input_file)
+  input = readdlm(input_file, String)
+  # convert 1D matrix of strings to 2D matrix of ints
+  # https://discourse.julialang.org/t/converting-a-array-of-strings-to-an-array-of-char/35123/2
+  input = reduce(vcat, permutedims.(collect.(input)))
+  parse.(Int, input)
+end
+
+function main()
+  main_input = read_input("../data/day11.txt")
+  test_input = read_input("../data/day11-test.txt")
+
+  @assert part1(test_input) == 1656
+  @assert part2(test_input) == 195
+
+  @show part1(main_input) # 1640
+  @show part2(main_input) # 312
+end
+
+@time main()
---
+++ 2021/src/template.jl
@@ -0,0 +1,28 @@
+using DelimitedFiles
+# using Memoize
+# using Combinatorics
+
+# Puzzle description: https://adventofcode.com/2021/day/XX
+
+
+
+function part1(input) end
+
+function part2(input) end
+
+function read_input(input_file)
+  readdlm(input_file, String)
+end
+
+function main()
+  main_input = read_input("../data/dayXX.txt")
+  test_input = read_input("../data/dayXX-test.txt")
+
+  @assert part1(test_input) == 0
+  # @assert part2(test_input) == 0
+
+  # @show part1(main_input) # 
+  # @show part2(test_input) # 
+end
+
+@time main()
