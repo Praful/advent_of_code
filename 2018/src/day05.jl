ea5eb8c3afdc4ad9d437041c307c05a027f55856
Praful Kapadia <git@kaptech.co.uk>
2022-04-03 20:36:30 +0100

    2018 day 6 done

--- 2018/src/day05.jl
+++ 2018/src/day05.jl
@@ -3,16 +3,19 @@ using Test
 
 # Puzzle description: https://adventofcode.com/2018/day/05
 
-const CHAR_LOWER_UPPER_GAP = 32
+# I was expecting to have to use a linked list but Julia turns out to 
+# be fast deleting elements in large vectors. The total run time is 0.3 secs.
+
+const LOWER_UPPER_GAP = abs('a' - 'A')
 
 function react(input)
+  opp_polarity(c1, c2) = abs(c1 - c2) == LOWER_UPPER_GAP
+
   polymer = collect(input)
   i = 1
 
   while i < length(polymer)
-    ((i + 1) > length(polymer)) && break
-
-    if abs(polymer[i] - polymer[i+1]) == CHAR_LOWER_UPPER_GAP
+    if opp_polarity(polymer[i], polymer[i+1])
       deleteat!(polymer, i)
       deleteat!(polymer, i)
       i = i == 1 ? 1 : i - 1
@@ -21,16 +24,25 @@ function react(input)
     end
   end
 
-  polymer
+  length(polymer)
 end
 
 function part1(input)
-  length(react(input))
+  react(input)
 end
 
 function part2(input)
-  pairs = map(a -> [a, uppercase(a)], 'a':'z')
-  minimum(map(p -> length(react(filter(e -> e ∉ p, input))), pairs))
+  letter_pairs = map(a -> [a, uppercase(a)], 'a':'z')
+  new_polymer(pair) = filter(letter -> letter ∉ pair, input)
+
+  # The commented out lines are other ways to express the same
+  # solution. These are experiments in piping and broadcasting.
+  # See https://docs.julialang.org/en/v1/manual/functions/
+  # map(pair -> react(new_polymer(pair)), letter_pairs) |> minimum
+  # [react(new_polymer(pair)) for pair in letter_pairs] |> minimum
+  # react.(new_polymer.(letter_pairs)) |> minimum
+
+  letter_pairs .|> new_polymer .|> react |> minimum
 end
 
 function read_input(input_file)
---
+++ 2018/src/day06.jl
@@ -0,0 +1,81 @@
+using AdventOfCodeUtils
+using DelimitedFiles
+using Test
+# using Memoize
+# using Combinatorics
+
+# Puzzle description: https://adventofcode.com/2018/day/06
+
+manhattan_distance(x1, y1, x2, y2) = abs(x1 - x2) + abs(y1 - y2)
+
+const MULTIPLE = -1
+const UNPROCESSED = 0
+
+function solve(input, max_distance)
+  cols = maximum(input[:, 1])
+  rows = maximum(input[:, 2])
+
+  # grid was defined for debugging
+  # grid = fill(UNPROCESSED, (rows, cols))
+  # [grid[c[2], c[1]] = n for (n, c) in enumerate(eachrow(input))]
+
+  closest_count = Dict()
+  max_region = 0
+
+  perimeter = Set()
+
+  for r in 1:rows, c in 1:cols
+    min_dist = Inf
+    points_found = 0
+    point_id = 0
+    total_dist = 0
+
+    for (n, p) in enumerate(eachrow(input))
+      dist = manhattan_distance(c, r, p[1], p[2])
+      total_dist += dist
+      if dist == min_dist
+        points_found += 1
+      elseif dist < min_dist
+        min_dist = dist
+        points_found = 1
+        point_id = n
+      end
+    end
+
+    if total_dist < max_distance
+      max_region += 1
+    end
+
+    on_perimeter(r, c) = r == 1 || r == rows || c == 1 || c == cols
+
+    if points_found == 1
+      # grid[r, c] = point_id
+      area = get(closest_count, point_id, 1)
+      closest_count[point_id] = area + 1
+      on_perimeter(r, c) && push!(perimeter, point_id)
+    elseif points_found > 1
+      # grid[r, c] = MULTIPLE
+    end
+  end
+
+  # println.(eachrow(grid))
+
+  # take away 1 because we increment count for when we get dist from
+  # location to itself 
+  maximum(kv -> kv[1] in perimeter ? 0 : kv[2], closest_count) - 1, max_region
+end
+
+function read_input(input_file)
+  readdlm(input_file, ',', Int)
+end
+
+function main()
+  main_input = read_input("../data/day06.txt")
+  test_input = read_input("../data/day06-test.txt")
+
+  @test solve(test_input, 32) == (17, 16)
+
+  @show solve(main_input, 10_000) # 5941, 40244
+end
+
+@time main()
