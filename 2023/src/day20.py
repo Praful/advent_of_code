f7b9b38ac8ae0be1beab986cba24adfe7272dd7a
praful <220836+Praful@users.noreply.github.com>
2024-01-06 22:30:04 +0000

    2023 day 20

--- 2023/src/day20.py
+++ 2023/src/day20.py
@@ -4,6 +4,7 @@ import os
 import sys
 import queue
 from pprint import pprint
+from enum import Enum
 
 sys.path.append(os.path.relpath("../../shared/python"))
 
@@ -12,12 +13,25 @@ from utils import *  # noqa: E402
 
 # Puzzle description: https://adventofcode.com/2023/day/20
 
+# For part 2, this uses a similar approach to day 8. The assumption, which
+# turns out to be true, is that the modules inputting to the rx module each
+# have their own repeating cycle. LCM is then used once we've found the cycle
+# length.
 
-# state: True: on or high pulse, False: off or low pulse
 Module = namedtuple('Module', ['id', 'type', 'destination', 'state'])
 
-HIGH = True
-LOW = False
+
+class Type(Enum):
+    FLIP_FLOP = '%'
+    CONJUNCTION = '&'
+
+
+class Pulse(Enum):
+    HIGH = True
+    LOW = False
+
+    @classmethod
+    def flip(cls, pulse): return cls(not pulse.value)
 
 
 def read_input(input_file):
@@ -29,13 +43,13 @@ def read_input(input_file):
         destination = s[1].split(', ')
         if l.startswith('broadcaster'):
             modules[s[0]] = Module(
-                'broadcaster', 'broadcaster', destination, LOW)
+                'broadcaster', 'broadcaster', destination, Pulse.LOW)
         else:
             name = s[0][1:]
-            type = s[0][0]
-            modules[name] = Module(name, type, destination, LOW)
+            type = Type(s[0][0])
+            modules[name] = Module(name, type, destination, Pulse.LOW)
             for d in destination:
-                conj_inputs[d][name] = LOW
+                conj_inputs[d][name] = Pulse.LOW
 
     return modules, conj_inputs
 
@@ -47,69 +61,73 @@ def solve(input, part1=True):
         else:
             return '-low->'
 
-    def update_queue(receiver, pulse):
-        for d in receiver.destination:
-            q.put((receiver.id, d, pulse))
+    def send_pulses(receiver, pulse):
+        for dest in receiver.destination:
+            q.put((receiver.id, dest, pulse))
 
     #  pprint(input)
     modules, conj_inputs = input
-    high = low = 0
     broadcaster = modules['broadcaster']
 
+    q = queue.SimpleQueue()
+    pulse_count = {Pulse.HIGH: 0, Pulse.LOW: 0}
+    result1 = result2 = None
+
+    # part 2 vars
     first_cycle = {}
     second_cycle = {}
-    result1 = result2 = None
     rx_sender = conj_inputs['rx']
-    q = queue.SimpleQueue()
-    t = 0
 
+    pushes = 0
     while True:
-        t += 1
-        if part1 and t > 1000:
-            result1 = low * high
+        pushes += 1
+        if part1 and pushes > 1000:
+            result1 = pulse_count[Pulse.HIGH] * pulse_count[Pulse.LOW]
             break
 
-        low += 1  # button press
-        for receiver_id in broadcaster.destination:
-            q.put((broadcaster.id, receiver_id, LOW))
+        pulse_count[Pulse.LOW] += 1  # button press
+        send_pulses(broadcaster, Pulse.LOW)
 
         while not q.empty():
             sender, receiver_id, pulse = q.get()
             #  print(sender, tr(pulse), receiver_id)
-            if pulse:
-                high += 1
-            else:
-                low += 1
+            pulse_count[pulse] += 1
 
             if receiver_id not in modules:
                 continue
 
             receiver = modules[receiver_id]
 
-            if receiver.type == '%':
-                if pulse == LOW:  # high pulses are ignored
-                    new_pulse = not receiver.state
-                    modules[receiver_id] = receiver._replace(state=new_pulse)
-                    update_queue(receiver, new_pulse)
+            match receiver.type:
+                case Type.FLIP_FLOP:
+                    if pulse == Pulse.LOW:  # high pulses are ignored
+                        new_pulse = Pulse.flip(receiver.state)
+                        modules[receiver_id] = receiver._replace(
+                            state=new_pulse)
+                        send_pulses(receiver, new_pulse)
 
-            elif receiver.type == '&':
-                inputs = conj_inputs[receiver.id]
-                inputs[sender] = pulse
+                case Type.CONJUNCTION:
+                    inputs = conj_inputs[receiver.id]
+                    inputs[sender] = pulse
 
-                if not part1 and receiver.id in rx_sender and pulse == HIGH:
-                    #  print(t, receiver_id, inputs)
-                    if sender not in first_cycle:
-                        first_cycle[sender] = t
-                    elif sender not in second_cycle:
-                        second_cycle[sender] = t
+                    if not part1 and receiver.id in rx_sender and pulse == Pulse.HIGH:
+                        #  print(t, receiver_id, inputs)
+                        if sender not in first_cycle:
+                            first_cycle[sender] = pushes
+                        elif sender not in second_cycle:
+                            second_cycle[sender] = pushes
 
-                    if len(second_cycle) == len(inputs):
-                        result2 = lcm(second_cycle[s] - first_cycle[s]
-                                      for s in second_cycle.keys())
-                        break
+                        if len(second_cycle) == len(inputs):
+                            result2 = lcm(second_cycle[s] - first_cycle[s]
+                                          for s in second_cycle.keys())
+                            break
 
-                new_pulse = not all(r == HIGH for r in inputs.values())
-                update_queue(receiver, new_pulse)
+                    new_pulse = Pulse(
+                        not all(r == Pulse.HIGH for r in inputs.values()))
+                    send_pulses(receiver, new_pulse)
+
+                case _:
+                    raise Exception(f'Unknown type {receiver.type}')
 
         if result2 is not None:
             break
@@ -126,6 +144,8 @@ def main():
     assert (res := solve(test_input2))[0] == 11687500, f'Actual: {res}'
     print(f'Part 1 {solve(input)[0]}')  # 684125385  (16195 x 42243)
 
+    # reset input
+    input = read_input("../data/day20.txt")
     print(f'Part 2 {solve(input, False)[1]}')  # 225872806380073
 
 
